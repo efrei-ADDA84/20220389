800f13adaa50af5bf65dfe74bf826f4e9197aac0
Suvéta <anandarajsuveta@gmail.com>
2023-04-28 15:32:21 +0200

    Modification de l'architecture et APIWrapper

---
+++ tp2/WeatherApiWrapper.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+from sys import argv
+
+
+
+app = Flask(__name__)
+
+
+ 
+
+@app.route("/", methods= ["GET"])
+def result() :
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={argv[1]}"
+    
+    return requests.get(url).json()
+    
+
+
+if __name__ == '__main__' :
+    app.run(debug=True, port=8081)
+
+
